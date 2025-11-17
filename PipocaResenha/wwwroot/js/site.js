3f3d6ffd026ce60fc1d1dcd4ff37f639da9729f5
maicon.clemer <maicon.clemer@BRE203D075658>
2025-11-17 20:16:53 -0300

    adicionado carrossel

--- PipocaResenha/wwwroot/js/site.js
+++ PipocaResenha/wwwroot/js/site.js
@@ -2,3 +2,34 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+<script>
+        document.querySelectorAll('.carrossel-filmes').forEach(carrossel => {
+          const linha = carrossel.querySelector('.filme-linha');
+    const esquerda = carrossel.querySelector('.seta-esquerda');
+    const direita = carrossel.querySelector('.seta-direita');
+    const scrollStep = 400;
+          esquerda.addEventListener('click', () => {
+        linha.scrollBy({ left: -scrollStep, behavior: 'smooth' });
+          });
+          direita.addEventListener('click', () => {
+        linha.scrollBy({ left: scrollStep, behavior: 'smooth' });
+          });
+          // Rolagem automática
+          let autoScroll = setInterval(() => {
+        linha.scrollBy({ left: scrollStep, behavior: 'smooth' });
+            if (linha.scrollLeft + linha.clientWidth >= linha.scrollWidth) {
+        linha.scrollTo({ left: 0, behavior: 'smooth' });
+            }
+          }, 4000);
+          carrossel.addEventListener('mouseenter', () => clearInterval(autoScroll));
+          carrossel.addEventListener('mouseleave', () => {
+        autoScroll = setInterval(() => {
+            linha.scrollBy({ left: scrollStep, behavior: 'smooth' });
+            if (linha.scrollLeft + linha.clientWidth >= linha.scrollWidth) {
+                linha.scrollTo({ left: 0, behavior: 'smooth' });
+            }
+        }, 4000);
+          });
+        });
+</script>
